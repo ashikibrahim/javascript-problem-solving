fad8c841cb74e09ceb0466dacd7f90756923cc2a
ashikibrahim <aashiqkibrahim@gmail.com>
2022-10-31 21:30:06 +0530

    sort return compare

--- frequency.js
+++ frequency.js
@@ -60,6 +60,41 @@
 // evCar.start();
 // evCar.end();
 // evCar.dashboard();
-
+// ----------------------------------------------------------------------------------
 
 // 4)...find duplicates in given array>>>>>>>>>>>>>>>>>>>>
+//  const duplicatedArray =[14,23,5,5,7,7,98,14,55]
+
+//  const duplicates = duplicatedArray.filter((ele,index,arr)=>arr.indexOf(ele)!== index)
+//  console.log(duplicates);
+// ---------------------------------------------------------------------------------
+
+// 5)count of duplicates in array
+// const months = ["may","april","june","may","feb","april","feb","july"]
+
+// const countOfDuplicates = months.reduce((obj,month)=>{
+//     if(obj[month]==undefined){
+//         obj[month]=1;
+//         return obj;
+//     }else{
+//         obj[month]++;
+//         return obj;
+//     }
+// },{})
+
+// console.log(countOfDuplicate
+// -------------------------------------------------
+
+// 6).>>>>>>>>>>>>>>>find the no is intiger
+// const validate = "ashik";
+//  console.log(!isNaN(validate))
+//-----------------------------------------------------
+
+// 7)sort an array>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+// const unsorted=[10,4,7,2,22,13];
+
+// unsorted.sort((a,b)=>{
+//     return a-b
+// })
+// console.log(unsorted);
+
