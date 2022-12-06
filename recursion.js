9e73449f9c517d44d3550829aad01a6e23283223
ashikibrahim <aashiqkibrahim@gmail.com>
2022-12-06 18:08:11 +0530

    recursion

---
+++ recursion.js
@@ -0,0 +1,23 @@
+// print numbers
+
+// function countDown(fromNumber){
+//     console.log(fromNumber,"aaaa");
+//     let nextNumber= fromNumber-1
+
+//     if(nextNumber>0){
+//     console.log(nextNumber,"bbbb");
+//         countDown(nextNumber)
+//     }
+// }
+// countDown(5)
+
+
+// factorial 
+ function factorial(n){
+    if(n==0){
+        return 1
+    }
+        return factorial(n-1)*n
+ }
+console.log(factorial(5));
+console.log(factorial(7));
