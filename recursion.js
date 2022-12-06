935257b18b6f9c623e3dc3f3529ee13d63747a59
ashikibrahim <aashiqkibrahim@gmail.com>
2022-12-06 23:03:19 +0530

    ss

--- recursion.js
+++ recursion.js
@@ -12,12 +12,13 @@
 // countDown(5)
 
 
+
 // factorial 
- function factorial(n){
-    if(n==0){
-        return 1
-    }
-        return factorial(n-1)*n
- }
-console.log(factorial(5));
-console.log(factorial(7));
+//  function factorial(n){
+//     if(n==0){
+//         return 1
+//     }
+//         return factorial(n-1)*n
+//  }
+// console.log(factorial(5));
+// console.log(factorial(7));
