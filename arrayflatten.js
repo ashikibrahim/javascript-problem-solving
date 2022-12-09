f09f024cb3495479f9b6bd51c1a608381f1877a0
ashikibrahim <aashiqkibrahim@gmail.com>
2022-12-10 00:03:29 +0530

    aaa

---
+++ arrayflatten.js
@@ -0,0 +1,6 @@
+// let nested=[[1,2],[1,3,[5,6]],[7,8]]
+
+// let arr2=nested.flat(Infinity)
+// console.log(arr2);
+
+([{id:1,name:"ameen",place:"clt",salary:"20000",gender:"male",age:26},{id:2,name:"ashik",place:"clt",salary:"20000",gender:"male",age:23}])
